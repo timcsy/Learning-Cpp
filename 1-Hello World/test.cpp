5037f3e57ef4ce307e6591e46179f1e6dee1664a
timcsy <timocsy@yahoo.com.tw>
2018-07-15 14:14:50 +0800

    modify test.cpp

--- 1-Hello World/test.cpp
+++ 1-Hello World/test.cpp
@@ -7,6 +7,7 @@
 //-----------------標頭檔區 (Header)-----------------//
 // #include 是引用別人寫好的一些東西
 #include <iostream> // iostream 意思是 input output stream 的縮寫，是負責處理輸入輸出的
+using namespace std; // 這行就是說使用standard(標準)的名稱空間(避免變數重複定義)
 
 //------------------主程式區 (Main)------------------//
 // int 是 integer(整數)，int main(){} 是程式的進入點
@@ -15,6 +16,6 @@ int main() {
 	// endl 是 end of line(換行)，需要在標頭檔區引入 iostream 才能使用
 	// 字串是用""包圍的一串文字
 	// << 是資料的流向
-	cout << "Hello World" << endl; // 寫C++的程式每個敘述(statement)最後一定要加分號(;)
+	cout << "Hello World嗨" << endl; // 寫C++的程式每個敘述(statement)最後一定要加分號(;)
 	return 0; // 告訴呼叫者(命令提示字元)說我執行完了
 }
