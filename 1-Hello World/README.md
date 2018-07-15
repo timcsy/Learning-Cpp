036cbe5f8dad9c46bde95910380ce18d1dac3e5c
timcsy <timocsy@yahoo.com.tw>
2018-07-15 13:41:26 +0800

    1-Hello World init

---
+++ 1-Hello World/README.md
@@ -0,0 +1,36 @@
+1-Hellow World
+===
+
+步驟
+---
+1. 新增一個資料夾
+2. 在編輯器中打開資料夾
+3. 在資料夾下新增一個檔案 test.cpp
+4. 將以下程式碼複製到 test.cpp 中
+	```
+	#include <iostream>
+	using namespace std;
+
+	int main() {
+		cout << "Hello World\n" << endl;
+		return 0;
+	}
+	```
+5. 打開命令提示字元(要切換到該資料夾)，輸入
+	```
+	g++ -o test test.cpp
+	```
+	來編譯程式
+6. 再輸入
+	```
+	.\test.exe
+	```
+	來執行程式
+7. 最後可以看到結果
+	```
+	> Hello World
+	```
+
+說明
+---
+可以參考上面test.c裡面的註解
---
+++ 1-Hello World/test.cpp
@@ -0,0 +1,20 @@
+// 我是單行註解
+
+/* 我是多行註解
+   第二行
+	 第三行  */
+
+//-----------------標頭檔區 (Header)-----------------//
+// #include 是引用別人寫好的一些東西
+#include <iostream> // iostream 意思是 input output stream 的縮寫，是負責處理輸入輸出的
+
+//------------------主程式區 (Main)------------------//
+// int 是 integer(整數)，int main(){} 是程式的進入點
+int main() {
+	// cout 是 C++ output(C++的輸出)，需要在標頭檔區引入 iostream 才能使用
+	// endl 是 end of line(換行)，需要在標頭檔區引入 iostream 才能使用
+	// 字串是用""包圍的一串文字
+	// << 是資料的流向
+	cout << "Hello World" << endl; // 寫C++的程式每個敘述(statement)最後一定要加分號(;)
+	return 0; // 告訴呼叫者(命令提示字元)說我執行完了
+}
